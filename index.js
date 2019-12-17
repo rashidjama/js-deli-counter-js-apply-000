4d4016ed581d1d3e30912e490a93e3716cc60a94
Rashid Jama <rashidjama5@gmail.com>
2019-12-17 02:09:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ function takeANumber(katzLine, customer) {
   return `Welcome, ${customer}. You are number ${katzLine.length} in line.`
 }
 
-let katzDeli = [];
-function nowServing(katzDeli, name) {
-  katzDeli.push(name)
-  if(katzDeli.length === []) {
-    return 'no one'
+function nowServing(katzDeli) {
+  if(katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
   } else {
-  return `${katzDeli.slice(0,1)}`
+    return `${katzDeli.slice(0,1)}`
   }
 }
-
-nowServing(katzDeli);
