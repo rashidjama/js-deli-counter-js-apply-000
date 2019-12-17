f8940eef2f517e08dfb3b879d9603e0b86c36bbd
Rashid Jama <rashidjama5@gmail.com>
2019-12-17 01:39:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function takeANumber(katzLine, customer) {
   katzLine.push(customer);
   return `Welcome, ${customer}. You are number ${katzLine.length} in line.`
 }
+
+let katzDeli = [];
+function nowServing(katzDeli, name) {
+  katzDeli.push(name)
+  if(katzDeli.length === []) {
+    return 'no one'
+  } else {
+  return `${katzDeli.slice(0,1)}`
+  }
+}
+
+nowServing(katzDeli);
